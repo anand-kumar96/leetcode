6ca0af24f98d20a8fad1c2afc0ab5f49190ced27
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-07-06 12:05:46 +0530

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -22,11 +22,11 @@ Constraints:
 -109 <= nums[i] <= 109
 -109 <= target <= 109
 Only one valid answer exists.
-*/
+
 method 01--> Brute Force
 apporach--> Loop through each element x of array and find if there is another value that equals to target - x.
 code-->
-
+*/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -39,12 +39,13 @@ class Solution {
         return null;  // In case there is no solution, we'll just return null
     }
 }
+/*
 Complexity Analysis
 Time complexity: O(n^2). For each element, we try to find its complement by looping through the rest of the array which
 takes O(n) time. Therefore, the time complexity is O(n^2).
 Space complexity: O(1). The space required does not depend on the size of the input array, so only constant space is used.
-
-
+   */
+/*
 method 02--> Two-pass Hash Table
 apporach-->A simple implementation uses two iterations. In the first iteration, we add each element's value as a key 
 and its index as a value to the hash table. Then, in the second iteration, we check if each element's
@@ -52,6 +53,7 @@ complement (target - nums[i]) exists in the hash table. If it does exist, we ret
 Beware that the complement must not be nums[i] itself!
 
 code-->
+    */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -67,6 +69,7 @@ class Solution {
         return null;   // In case there is no solution, we'll just return null
     }
 }
+/*
 Complexity Analysis
 
 Time complexity:O(n). We traverse the list containing nn elements exactly twice. 
@@ -74,13 +77,15 @@ Since the hash table reduces the lookup time to O(1), the overall time complexit
 
 Space complexity: O(n). The extra space required depends on the number of items stored in the hash table,
 which stores exactly nn elements.
-
+    */
+/*
 method 03: One-pass Hash Table
 apporach--> It turns out we can do it in one-pass. While we are iterating and inserting elements into the hash table,
 we also look back to check if current element's complement already exists in the hash table.
 If it exists, we have found a solution and return the indices immediately.
 
 code-->
+    */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -94,6 +99,8 @@ class Solution {
         return null;       // In case there is no solution, we'll just return null
     }
 }
+/*
 Time complexity:O(n). We traverse the list containing nn elements only once. Each lookup in the table costs only O(1) time.
 
 Space complexity:O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
+        */
