984f5f53dbe064d595cad96931786d72b257ca79
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-07-26 19:13:28 +0530

    Update twosum.java

--- problemSolution/array/twosum.java
+++ problemSolution/array/twosum.java
@@ -95,3 +95,21 @@ class Solution {
 Time complexity:O(n). We traverse the list containing nn elements only once. Each lookup in the table costs only O(1) time.
 Space complexity:O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
         */
+
+// 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+     int n=nums.length;
+        int res[]=new int[2];
+        for(int i=0;i<n;i++){
+          for(int j=i+1;j<n;j++){
+              if(nums[i]+nums[j]==target){
+                  res[0]=i;
+                  res[1]=j;
+              }
+          }
+        }
+        
+        return res;
+    }
+}
