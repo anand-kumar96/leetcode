548d13a7e16d86e5a8b53abdad30eb0458a9d2b1
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-08-19 23:54:42 +0530

    Update 1108. Defanging an IP Address.java

--- String/1108. Defanging an IP Address.java
+++ String/1108. Defanging an IP Address.java
@@ -1,18 +1,20 @@
 class Solution {
     public String defangIPaddr(String address) {
     // method 01 
-        // String str[]=address.split("\\.");
-        // int n=str.length;
-        // System.out.println(n);
-        // if(n==0){
-        //     return null;
-        // }
-        // String ans="";
-        // for(int i=0;i<n-1;i++){
-        //     ans=ans+str[i]+"[.]";
-        // }
-        // ans=ans+str[n-1];
-        // return ans;
+        String str[]=address.split("\\.");
+        int n=str.length;
+        System.out.println(n);
+        if(n==0){
+            return null;
+        }
+        String ans="";
+        for(int i=0;i<n-1;i++){
+            ans=ans+str[i]+"[.]";
+        }
+        ans=ans+str[n-1];
+        return ans;
+        
+        
 //         String str[]=address.split("\\.");
 //         int n=str.length;
 //         if(n==0){
