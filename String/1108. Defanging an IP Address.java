741f0da74808c9b82a893fdd9e73ffc9c4518dc3
Anand kumar <106487247+aryananand01@users.noreply.github.com>
2022-08-19 23:54:19 +0530

    Create 1108. Defanging an IP Address.java

---
+++ String/1108. Defanging an IP Address.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String defangIPaddr(String address) {
+    // method 01 
+        // String str[]=address.split("\\.");
+        // int n=str.length;
+        // System.out.println(n);
+        // if(n==0){
+        //     return null;
+        // }
+        // String ans="";
+        // for(int i=0;i<n-1;i++){
+        //     ans=ans+str[i]+"[.]";
+        // }
+        // ans=ans+str[n-1];
+        // return ans;
+//         String str[]=address.split("\\.");
+//         int n=str.length;
+//         if(n==0){
+//             return null;
+//         }
+//         String ans="";
+//         for(int i=0;i<n;i++){
+//             ans=ans+str[i]+"[.]";
+//         }
+//         ans=ans.substring(0,ans.length()-3);
+//         return ans;
+
+
+// method 02
+        String str[]=address.split("\\.");
+        int n=str.length;
+        if(n==0){
+            return null;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<n-1;i++){
+            sb.append(str[i]);
+            sb.append("[.]");
+        }
+        sb.append(str[n-1]);
+        return sb.toString();
+        
+        
+    }
+}
